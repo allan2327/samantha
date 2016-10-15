6a240bfda12c9a9be58c9de2bc04df18e9a2fea0
Vasco Correia <maxteel1@gmail.com>
2016-10-15 22:32:16 +0100

    adapting the pattern lib chunker

--- cenas.py
+++ cenas.py
@@ -5,23 +5,23 @@ from textblob import TextBlob
 
 from samantha.nlputils import TextBlobExtended
 
-s1 = "Sometimes I can't see crows buzzing around the tree trunks in my iron-clad yellowed pajamas and boots. Don't' fish a fish."
+s1 = "Sometimes I can't see crows buzzing around the tree trunks in my iron-clad yellowed pajamas and boots." #" Don't' fish a fish."
 s2 = "I look at the trees dancing."
 
 a = TextBlobExtended(s1)
 
 
-for x in a.words:
-    print(x, x.pos_tag)
-print('')
+# for x in a.words:
+#     print(x, x.pos_tag)
+# print('')
 
-for x in a.tokens:
-    print(x, x.pos_tag)
-print('')
+# for x in a.tokens:
+#     print(x, x.pos_tag)
+# print('')
 
-for x in a.pos_tags:
-    print(x[0], x[0].pos_tag, x[1])
-print('')
+# for x in a.pos_tags:
+#     print(x[0], x[0].pos_tag, x[1])
+# print('')
 
 # for x in a.pos_tags_with_punctuation:
 #     print(x)
@@ -39,28 +39,16 @@ RB = r"(?<!W)RB|RBR|RBS"
 # Chunking rules.
 # CHUNKS[0] = Germanic: RB + JJ precedes NN ("the round table").
 # CHUNKS[1] = Romance: RB + JJ precedes or follows NN ("la table ronde", "une jolie fille").
-CHUNKS = [[
+CHUNKS = [
     # Germanic languages: en, de, nl, ...
     (  "NP", re.compile(r"(("+NN+")/)*((DT|CD|CC|CJ)/)*(("+RB+"|"+JJ+")/)*(("+NN+")/)+")),
+    (  "PP", re.compile(r"((IN|PP|TO)/)+")),
     (  "VP", re.compile(r"(((MD|"+RB+")/)*(("+VB+")/)+)+")),
     (  "VP", re.compile(r"((MD)/)")),
-    (  "PP", re.compile(r"((IN|PP|TO)/)+")),
     ("ADJP", re.compile(r"((CC|CJ|"+RB+"|"+JJ+")/)*(("+JJ+")/)+")),
     ("ADVP", re.compile(r"(("+RB+"|WRB)/)+")),
-], [
-    # Romance languages: es, fr, it, ...
-    (  "NP", re.compile(r"(("+NN+")/)*((DT|CD|CC|CJ)/)*(("+RB+"|"+JJ+")/)*(("+NN+")/)+(("+RB+"|"+JJ+")/)*")),
-    (  "VP", re.compile(r"(((MD|"+RB+")/)*(("+VB+")/)+(("+RB+")/)*)+")),
-    (  "VP", re.compile(r"((MD)/)")),
-    (  "PP", re.compile(r"((IN|PP|TO)/)+")),
-    ("ADJP", re.compile(r"((CC|CJ|"+RB+"|"+JJ+")/)*(("+JJ+")/)+")),
-    ("ADVP", re.compile(r"(("+RB+"|WRB)/)+")),
-]]
+]
 
-# Handle ADJP before VP, so that
-# RB prefers next ADJP over previous VP.
-CHUNKS[0].insert(1, CHUNKS[0].pop(3))
-CHUNKS[1].insert(1, CHUNKS[1].pop(3))
 
 def find_chunks(tagged, language="en"):
     """ The input is a list of [token, tag]-items.
@@ -69,9 +57,9 @@ def find_chunks(tagged, language="en"):
         The/DT/B-NP nice/JJ/I-NP fish/NN/I-NP is/VBZ/B-VP dead/JJ/B-ADJP ././O
     """
     chunked = [x for x in tagged]
-    tags = "".join("%s%s" % (tag, SEPARATOR) for token, tag in tagged)
+    tags = "".join("%s%s" % (token.pos_tag, SEPARATOR) for token in tagged)
     # Use Germanic or Romance chunking rules according to given language.
-    for tag, rule in CHUNKS[int(language in ("ca", "es", "pt", "fr", "it", "pt", "ro"))]:
+    for tag, rule in CHUNKS:
         for m in rule.finditer(tags):
             # Find the start of chunks inside the tags-string.
             # Number of preceding separators = number of preceding tokens.
@@ -79,28 +67,27 @@ def find_chunks(tagged, language="en"):
             j = tags[:i].count(SEPARATOR)
             n = m.group(0).count(SEPARATOR)
             for k in range(j, j+n):
-                if len(chunked[k]) == 3:
+                if chunked[k].chunk:
                     continue
-                if len(chunked[k]) < 3:
-                    # A conjunction can not be start of a chunk.
-                    if k == j and chunked[k][1] in ("CC", "CJ", "KON", "Conj(neven)"):
-                        j += 1
-                    # Mark first token in chunk with B-.
-                    elif k == j:
-                        chunked[k].append("B-"+tag)
-                    # Mark other tokens in chunk with I-.
-                    else:
-                        chunked[k].append("I-"+tag)
+                # A conjunction can not be start of a chunk.
+                if k == j and chunked[k].pos_tag in ("CC", "CJ", "KON", "Conj(neven)"):
+                    j += 1
+                # Mark first token in chunk with B-.
+                elif k == j:
+                    chunked[k].chunk = "B-"+tag
+                # Mark other tokens in chunk with I-.
+                else:
+                    chunked[k].chunk = "I-"+tag
     # Mark chinks (tokens outside of a chunk) with O-.
     for chink in filter(lambda x: len(x) < 3, chunked):
-        chink.append("O")
+        chink.chunk = "O"
     # Post-processing corrections.
-    for i, (word, tag, chunk) in enumerate(chunked):
-        if tag.startswith("RB") and chunk == "B-NP":
+    for i, word in enumerate(chunked):
+        if word.pos_tag.startswith("RB") and word.chunk == "B-NP":
             # "Very nice work" (NP) <=> "Perhaps" (ADVP) + "you" (NP).
-            if i < len(chunked)-1 and not chunked[i+1][1].startswith("JJ"):
-                chunked[i+0][2] = "B-ADVP"
-                chunked[i+1][2] = "B-NP"
+            if i < len(chunked)-1 and not chunked[i+1].pos_tag.startswith("JJ"):
+                chunked[i+0].chunk = "B-ADVP"
+                chunked[i+1].chunk = "B-NP"
     return chunked
 
 def find_prepositions(chunked):
@@ -110,40 +97,36 @@ def find_prepositions(chunked):
     """
     # Tokens that are not part of a preposition just get the O-tag.
     for ch in chunked:
-        ch.append("O")
+        ch.preposition = "O"
     for i, chunk in enumerate(chunked):
-        if chunk[2].endswith("PP") and chunk[-1] == "O":
+        if chunk.chunk.endswith("PP") and chunk.preposition == "O":
             # Find PP followed by other PP, NP with nouns and pronouns, VP with a gerund.
             if i < len(chunked)-1 and \
-             (chunked[i+1][2].endswith(("NP", "PP")) or \
-              chunked[i+1][1] in ("VBG", "VBN")):
-                chunk[-1] = "B-PNP"
+             (chunked[i+1].chunk.endswith(("NP", "PP")) or \
+              chunked[i+1].pos_tag in ("VBG", "VBN")):
+                chunk.preposition = "B-PNP"
                 pp = True
                 for ch in chunked[i+1:]:
-                    if not (ch[2].endswith(("NP", "PP")) or ch[1] in ("VBG", "VBN")):
+                    if not (ch.chunk.endswith(("NP", "PP")) or ch.pos_tag in ("VBG", "VBN")):
                         break
-                    if ch[2].endswith("PP") and pp:
-                        ch[-1] = "I-PNP"
-                    if not ch[2].endswith("PP"):
-                        ch[-1] = "I-PNP"
+                    if ch.chunk.endswith("PP") and pp:
+                        ch.preposition = "I-PNP"
+                    if not ch.chunk.endswith("PP"):
+                        ch.preposition = "I-PNP"
                         pp = False
     return chunked
 
 
 
+b = [t[0]for t in a.pos_tags_with_punctuation]
 
-b = [[t[0].string, t[1]] for t in a.pos_tags]
-
-#print(find_prepositions(find_chunks(b)))
+print(b)
 print('')
-#print(a.parse().split(' '))
+print(find_prepositions(find_chunks(b)))
+print('')
+print(a.parse().replace(' ', '\n'))
 
-for i, j in zip(a.parse().split(' '), find_prepositions(find_chunks(b))):
-    print(i.split('/'), '   ', j)
+# for i, j in zip(a.parse().split(' '), find_prepositions(find_chunks(b))):
+#     print(i.split('/'), '   ', j)
 
 
-    
-    
-    
-    
-    
--- samantha/nlputils.py
+++ samantha/nlputils.py
@@ -1,14 +1,28 @@
 
 from textblob import TextBlob, WordList, Word
 from textblob.blob import _penn_to_wordnet
+from textblob.compat import unicode
+from textblob.utils import PUNCTUATION_REGEX
 from textblob.tokenizers import word_tokenize
 from textblob.decorators import cached_property
+
+
+
+class WordExtended(Word):
+
+    def __init__(self, string, pos_tag=None, chunk=None, preposition=None):
+        super(WordExtended, self).__init__(string, pos_tag)
+        self.chunk = chunk
+        self.preposition = preposition
+
+    def __repr__(self):
+        return "{0} {1} {2} {3}".format(self.string, self.pos_tag, self.chunk, self.preposition)
 
 
 class WordListExtended(WordList):
     
     def __init__(self, collection):
-        """ collection is a list of (Word, tag), that come from a WordBlol.pos_tags"""
+        """ collection is a list of (Word, tag), that come from a WordBlob.pos_tags"""
         
         self._collection = [w[0] for w in collection]
         super(WordList, self).__init__(self._collection)
@@ -20,10 +34,16 @@ class TextBlobExtended(TextBlob):
     def words(self):
         """ ensures every words has an associated pos tag """
         return WordListExtended(self.pos_tags)
+
+    @cached_property
+    def pos_tags(self):
+        return [(WordExtended(word, pos_tag=t), unicode(t))
+                for word, t in self.pos_tagger.tag(self.raw)
+                if not PUNCTUATION_REGEX.match(unicode(t))]
         
     @cached_property
     def pos_tags_with_punctuation(self):
-        return [(Word(word, pos_tag=t), unicode(t))
+        return [(WordExtended(word, pos_tag=t), unicode(t))
                 for word, t in self.pos_tagger.tag(self.raw)]
                 
     @cached_property
