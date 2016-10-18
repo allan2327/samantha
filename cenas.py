38b89402e64f5c8468e47f12e25425528e75cf85
Vasco Correia <maxteel1@gmail.com>
2016-10-18 09:24:50 +0100

    implemented chunking

--- cenas.py
+++ cenas.py
@@ -1,132 +1,31 @@
 
-import re
 import textblob
 from textblob import TextBlob
 
-from samantha.nlputils import TextBlobExtended
+from samantha.nlputils import TextBlobExtended, WordExtended
 
 s1 = "Sometimes I can't see crows buzzing around the tree trunks in my iron-clad yellowed pajamas and boots." #" Don't' fish a fish."
 s2 = "I look at the trees dancing."
-
-a = TextBlobExtended(s1)
-
-
-# for x in a.words:
-#     print(x, x.pos_tag)
-# print('')
-
-# for x in a.tokens:
-#     print(x, x.pos_tag)
-# print('')
-
-# for x in a.pos_tags:
-#     print(x[0], x[0].pos_tag, x[1])
-# print('')
-
-# for x in a.pos_tags_with_punctuation:
-#     print(x)
-# print('')
+s3 = "Python is a high-level programming language."
 
 
 
-SEPARATOR = "/"
-
-NN = r"NN|NNS|NNP|NNPS|NNPS?\-[A-Z]{3,4}|PR|PRP|PRP\$"
-VB = r"VB|VBD|VBG|VBN|VBP|VBZ"
-JJ = r"JJ|JJR|JJS"
-RB = r"(?<!W)RB|RBR|RBS"
-
-# Chunking rules.
-# CHUNKS[0] = Germanic: RB + JJ precedes NN ("the round table").
-# CHUNKS[1] = Romance: RB + JJ precedes or follows NN ("la table ronde", "une jolie fille").
-CHUNKS = [
-    # Germanic languages: en, de, nl, ...
-    (  "NP", re.compile(r"(("+NN+")/)*((DT|CD|CC|CJ)/)*(("+RB+"|"+JJ+")/)*(("+NN+")/)+")),
-    (  "PP", re.compile(r"((IN|PP|TO)/)+")),
-    (  "VP", re.compile(r"(((MD|"+RB+")/)*(("+VB+")/)+)+")),
-    (  "VP", re.compile(r"((MD)/)")),
-    ("ADJP", re.compile(r"((CC|CJ|"+RB+"|"+JJ+")/)*(("+JJ+")/)+")),
-    ("ADVP", re.compile(r"(("+RB+"|WRB)/)+")),
-]
+while False:
+    a = input('> ')
+    a = TextBlobExtended(a)
+    for w in a.chunks:
+        print('==> {}'.format(w))
 
 
-def find_chunks(tagged, language="en"):
-    """ The input is a list of [token, tag]-items.
-        The output is a list of [token, tag, chunk]-items:
-        The/DT nice/JJ fish/NN is/VBZ dead/JJ ./. =>
-        The/DT/B-NP nice/JJ/I-NP fish/NN/I-NP is/VBZ/B-VP dead/JJ/B-ADJP ././O
-    """
-    chunked = [x for x in tagged]
-    tags = "".join("%s%s" % (token.pos_tag, SEPARATOR) for token in tagged)
-    # Use Germanic or Romance chunking rules according to given language.
-    for tag, rule in CHUNKS:
-        for m in rule.finditer(tags):
-            # Find the start of chunks inside the tags-string.
-            # Number of preceding separators = number of preceding tokens.
-            i = m.start()
-            j = tags[:i].count(SEPARATOR)
-            n = m.group(0).count(SEPARATOR)
-            for k in range(j, j+n):
-                if chunked[k].chunk:
-                    continue
-                # A conjunction can not be start of a chunk.
-                if k == j and chunked[k].pos_tag in ("CC", "CJ", "KON", "Conj(neven)"):
-                    j += 1
-                # Mark first token in chunk with B-.
-                elif k == j:
-                    chunked[k].chunk = "B-"+tag
-                # Mark other tokens in chunk with I-.
-                else:
-                    chunked[k].chunk = "I-"+tag
-    # Mark chinks (tokens outside of a chunk) with O-.
-    for chink in filter(lambda x: len(x) < 3, chunked):
-        chink.chunk = "O"
-    # Post-processing corrections.
-    for i, word in enumerate(chunked):
-        if word.pos_tag.startswith("RB") and word.chunk == "B-NP":
-            # "Very nice work" (NP) <=> "Perhaps" (ADVP) + "you" (NP).
-            if i < len(chunked)-1 and not chunked[i+1].pos_tag.startswith("JJ"):
-                chunked[i+0].chunk = "B-ADVP"
-                chunked[i+1].chunk = "B-NP"
-    return chunked
-
-def find_prepositions(chunked):
-    """ The input is a list of [token, tag, chunk]-items.
-        The output is a list of [token, tag, chunk, preposition]-items.
-        PP-chunks followed by NP-chunks make up a PNP-chunk.
-    """
-    # Tokens that are not part of a preposition just get the O-tag.
-    for ch in chunked:
-        ch.preposition = "O"
-    for i, chunk in enumerate(chunked):
-        if chunk.chunk.endswith("PP") and chunk.preposition == "O":
-            # Find PP followed by other PP, NP with nouns and pronouns, VP with a gerund.
-            if i < len(chunked)-1 and \
-             (chunked[i+1].chunk.endswith(("NP", "PP")) or \
-              chunked[i+1].pos_tag in ("VBG", "VBN")):
-                chunk.preposition = "B-PNP"
-                pp = True
-                for ch in chunked[i+1:]:
-                    if not (ch.chunk.endswith(("NP", "PP")) or ch.pos_tag in ("VBG", "VBN")):
-                        break
-                    if ch.chunk.endswith("PP") and pp:
-                        ch.preposition = "I-PNP"
-                    if not ch.chunk.endswith("PP"):
-                        ch.preposition = "I-PNP"
-                        pp = False
-    return chunked
-
-
-
-b = [t[0]for t in a.pos_tags_with_punctuation]
-
-print(b)
+a = TextBlobExtended("I sleep in the bed.")
+print(a.chunks)
 print('')
-print(find_prepositions(find_chunks(b)))
-print('')
-print(a.parse().split(' '))
 
-# for i, j in zip(a.parse().split(' '), find_prepositions(find_chunks(b))):
-#     print(i.split('/'), '   ', j)
+w = a.words[1]
+print(w)
+print(w.pos_tag)
+print(w.lemma)
+print(w.synsets)
+print(w.definitions)
 
 
--- samantha/nlputils.py
+++ samantha/nlputils.py
@@ -1,11 +1,11 @@
 
-from textblob import TextBlob, WordList, Word
+import re
+from textblob import TextBlob, Word, WordList
 from textblob.blob import _penn_to_wordnet
 from textblob.compat import unicode
-from textblob.utils import PUNCTUATION_REGEX
-from textblob.tokenizers import word_tokenize
 from textblob.decorators import cached_property
-
+from textblob.tokenizers import word_tokenize
+from textblob.utils import PUNCTUATION_REGEX
 
 
 class WordExtended(Word):
@@ -15,8 +15,8 @@ class WordExtended(Word):
         self.chunk = chunk
         self.preposition = preposition
 
-    def __repr__(self):
-        return "{0} {1} {2} {3}".format(self.string, self.pos_tag, self.chunk, self.preposition)
+    # def __repr__(self):
+    #     return "{0} {1} {2} {3}".format(self.string, self.pos_tag, self.chunk, self.preposition)
 
 
 class WordListExtended(WordList):
@@ -24,34 +24,128 @@ class WordListExtended(WordList):
     def __init__(self, collection):
         """ collection is a list of (Word, tag), that come from a WordBlob.pos_tags"""
         
-        self._collection = [w[0] for w in collection]
+        self._collection = [w for w in collection]
         super(WordList, self).__init__(self._collection)
 
 
 class TextBlobExtended(TextBlob):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._words = None
+        self._tagged = False
+        self._chunked = False
+        
     
     @cached_property
     def words(self):
-        """ ensures every words has an associated pos tag """
-        return WordListExtended(self.pos_tags)
+        if not self._words:
+            self.pos_tags_with_punctuation
+        return WordListExtended(word for word in self._words
+                                if not PUNCTUATION_REGEX.match(word.pos_tag))
 
     @cached_property
     def pos_tags(self):
-        return [(WordExtended(word, pos_tag=t), unicode(t))
-                for word, t in self.pos_tagger.tag(self.raw)
-                if not PUNCTUATION_REGEX.match(unicode(t))]
+        return [tup for tup in self.pos_tags_with_punctuation
+                if not PUNCTUATION_REGEX.match(tup[1])]
         
     @cached_property
     def pos_tags_with_punctuation(self):
-        return [(WordExtended(word, pos_tag=t), unicode(t))
-                for word, t in self.pos_tagger.tag(self.raw)]
+        if not self._tagged:
+            self._words = WordListExtended(WordExtended(word, tag) 
+                                           for word, tag in self.pos_tagger.tag(self.raw))
+            self._tagged = True
+        return [(word, word.pos_tag) for word in self._words]
                 
     @cached_property
     def tokens(self):
-        return WordListExtended(self.pos_tags_with_punctuation)
+        if not self._words:
+            self.pos_tags_with_punctuation
+        return self._words
         
-    # TODO 
-    # def parse(self):
-    #    stuff
-    
-    
+    @cached_property
+    def chunks(self):
+        if not self._chunked:
+            if not self._tagged:
+                self.pos_tags_with_punctuation
+            chunk_words(self._words)
+            calculate_prepositions(self._words)
+            self._chunked = True
+        return [(word, word.pos_tag, word.chunk, word.preposition) for word in self._words]
+
+
+SEPARATOR = "/"
+
+NN = r"NN|NNS|NNP|NNPS|NNPS?\-[A-Z]{3,4}|PR|PRP|PRP\$"
+VB = r"VB|VBD|VBG|VBN|VBP|VBZ"
+JJ = r"JJ|JJR|JJS"
+RB = r"(?<!W)RB|RBR|RBS"
+
+# Chunking rules.
+CHUNKS_RULES = [
+    # Germanic languages: en, de, nl, ...
+    (  "NP", re.compile(r"(("+NN+")/)*((DT|CD|CC|CJ)/)*(("+RB+"|"+JJ+")/)*(("+NN+")/)+")),
+    (  "PP", re.compile(r"((IN|PP|TO)/)+")),
+    (  "VP", re.compile(r"(((MD|"+RB+")/)*(("+VB+")/)+)+")),
+    (  "VP", re.compile(r"((MD)/)")),
+    ("ADJP", re.compile(r"((CC|CJ|"+RB+"|"+JJ+")/)*(("+JJ+")/)+")),
+    ("ADVP", re.compile(r"(("+RB+"|WRB)/)+")),
+]
+
+def chunk_words(words, language="en"):
+    chunked_words = [x for x in words]
+    tags = "".join("%s%s" % (word.pos_tag, SEPARATOR) for word in words)
+    # Use Germanic or Romance chunking rules according to given language.
+    for tag, rule in CHUNKS_RULES:
+        for m in rule.finditer(tags):
+            # Find the start of chunks_RULES inside the tags-string.
+            # Number of preceding separators = number of preceding tokens.
+            i = m.start()
+            j = tags[:i].count(SEPARATOR)
+            n = m.group(0).count(SEPARATOR)
+            for k in range(j, j+n):
+                if chunked_words[k].chunk:
+                    continue
+                # A conjunction can not be start of a chunk.
+                if k == j and chunked_words[k].pos_tag in ("CC", "CJ", "KON", "Conj(neven)"):
+                    j += 1
+                # Mark first token in chunk with B-.
+                elif k == j:
+                    chunked_words[k].chunk = "B-"+tag
+                # Mark other tokens in chunk with I-.
+                else:
+                    chunked_words[k].chunk = "I-"+tag
+    # Mark chinks (tokens outside of a chunk) with O-.
+    for chink in filter(lambda x: x.chunk is None, chunked_words):
+        chink.chunk = "O"
+    # Post-processing corrections.
+    for i, word in enumerate(chunked_words):
+        if word.pos_tag.startswith("RB") and word.chunk == "B-NP":
+            # "Very nice work" (NP) <=> "Perhaps" (ADVP) + "you" (NP).
+            if i < len(chunked_words)-1 and not chunked_words[i+1].pos_tag.startswith("JJ"):
+                chunked_words[i+0].chunk = "B-ADVP"
+                chunked_words[i+1].chunk = "B-NP"
+    return chunked_words
+
+
+def calculate_prepositions(chunked_words):
+    # Tokens that are not part of a preposition just get the O-tag.
+    for word in chunked_words:
+        word.preposition = "O"
+    for i, word in enumerate(chunked_words):
+        if word.chunk.endswith("PP") and word.preposition == "O":
+            # Find PP followed by other PP, NP with nouns and pronouns, VP with a gerund.
+            if i < len(chunked_words)-1 and \
+             (chunked_words[i+1].chunk.endswith(("NP", "PP")) or \
+              chunked_words[i+1].pos_tag in ("VBG", "VBN")):
+                word.preposition = "B-PNP"
+                pp = True
+                for w in chunked_words[i+1:]:
+                    if not (w.chunk.endswith(("NP", "PP")) or w.pos_tag in ("VBG", "VBN")):
+                        break
+                    if w.chunk.endswith("PP") and pp:
+                        w.preposition = "I-PNP"
+                    if not w.chunk.endswith("PP"):
+                        w.preposition = "I-PNP"
+                        pp = False
+    return chunked_words
