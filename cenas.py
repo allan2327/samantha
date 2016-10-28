fa28f91389db74013848550cd989728482ddd0bb
Vasco Correia <maxteel1@gmail.com>
2016-10-28 01:26:59 +0100

    spacy and dependency analysis

--- cenas.py
+++ cenas.py
@@ -1,111 +1,59 @@
-import re
-import textblob
-from textblob import TextBlob
 
-from samantha.nlputils.wordblobextensions import TextBlobExtended, WordExtended
+from spacy.tokens.token import Token
+import spacy
+
+parser = spacy.load('en')
+
+doc = parser("There are bananas on the table")
+
+def print_sentence_tree(doc):
+    def get_depth(t, depth=0):
+        result = {}
+        result[t.idx] = depth
+        for c in t.children:
+            result.update(get_depth(c, depth+1))
+        return result
+
+    for root in doc:
+        if root.dep_ == "ROOT":
+            break
+
+    depths = get_depth(root)
+    for t in doc:
+        print('   ' * depths[t.idx], t.orth_, t.tag_, t.pos_, t.dep_)
 
 
-s1 = "Sometimes I can't see crows buzzing around the tree trunks in my iron-clad yellowed pajamas and boots." #" Don't' fish a fish."
-s2 = "I look at the trees dancing."
-s3 = "Python is a high-level programming language."
-s4 = "What day of the week is December fifth"
-
-
-ss = [s1, s2, s3, s4]
+print_sentence_tree(doc)
+print('')
 
 ss = [
-    "What time is it?",
-    "How many time until eight?",
     "What day is it?",
+    "How many time until eight?",
     "What day of the week is it?",
     "What day of the week is December fifth?",
     "When is the next holiday?",
     "What day of the week was twenty fourth of April nineteen eighty six?",
-    "I want to wake up in five and a half hours."
-]
-
-ss = [
-    "eight thousand and fifteen apples",
-    "minus seven",
-    "a cat",
-    "thirty seven",
-    "a million",
-    "seven and a half",
-    "five billion and three quarters",
-    "four million and three hundred cakes and four dogs",
-    "forty two and two half",
-    "75943 and a half",
-    "I like 3.5 bottles and 4 cakes"
+    "What time is it?",
+    "What is the time?",
+    "What day is it?",
+    "What is the day?"
 ]
 
 for s in ss:
-    a = TextBlobExtended(s)
-    a.relations
-    print(a)
-    for w in a._words:
-        print(repr(w))
-    print('')
-    
-
-import sys
-from samantha.nlputils.numbers import NumberService
-
-service = NumberService()
-
-# for s in ss:
-#     try:
-#         print(s)
-#         print(service.parse(s))
-#     except Exception:
-#         print(sys.exc_info())
-#     print('')
-
-
-def process_numbers(s):
-    a = s.split(' ')
-
-    result = []
-    current = []
-    for w in a:
-        if w in service.__small__ or \
-                w in service.__magnitude__ or \
-                w in service.__fractions__ or \
-                w == "hundred":
-            current.append(w)
-        elif w == "and":
-            continue
-        elif w == "a":
-            current.append(w)
-        elif current:
-            result.append(current)
-            current = []
-    else:
-        if current:
-            result.append(current)
-            current = []
-    return result
-
-
-## WARNING!!
-
-# >>> service.parse("a half")                                                                                                                                               
-# 3                                                                                                                                                                         
-# >>> service.parse("half")                                                                                                                                                 
-# 2                                                                                                                                                                         
-# >>> service.parse("seven and a half")                                                                                                                                     
-# 7.5                                                                                                                                                                       
-# >>> service.parse("seven a half")                                                                                                                                         
-# 10                                                                                                                                                                        
-# >>>    
-
-
-
-for s in ss:
-    print(s)
-    processed = process_numbers(s)
-    print(processed)
-    a = [' '.join(n) for n in processed]
-    a = [service.parse(n) for n in a]
-    print(a)
+    print("## " + s)
+    print_sentence_tree(parser(s))
     print('')
 
+
+def get_action(token, rule):
+    for rule_child for rule.children:
+        result = rule_child.evaluate(token)
+        if result.action:
+            return result
+
+def evaluate_rule(rule, token);
+    result = rule.evaluate(token)
+    if result:
+        return result
+    for child_rule in rule.children:
+        evaluate_rule(child_rule, token)
